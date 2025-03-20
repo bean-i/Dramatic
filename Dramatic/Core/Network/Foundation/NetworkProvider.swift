e797f6bf25f00df2e36949f9d6683c7ef87b26aa
김도형 <shapekim98@gmail.com>
2025-03-20 20:55:59 +0900

    [refactor] #1 네트워크 provider 옵저버블로 변경

--- Dramatic/Core/Network/Foundation/NetworkProvider.swift
+++ Dramatic/Core/Network/Foundation/NetworkProvider.swift
@@ -8,32 +8,41 @@
 import Foundation
 
 import Alamofire
+import RxCocoa
+import RxSwift
 
 struct NetworkProvider<E: EndPoint>: Sendable {
-    func request<T: Decodable>(_ endPoint: E) async throws -> T {
+    func request<T: Decodable>(_ endPoint: E) -> Single<T> {
+        return .create { observer in
+            let task = Task {
+                do {
 #if DEBUG
-        try NetworkLogger.request(endPoint)
+                    try NetworkLogger.request(endPoint)
 #endif
-        let response = await AF.request(endPoint)
-        .validate(statusCode: 200..<300)
-        .serializingDecodable(T.self, decoder: endPoint.decoder)
-        .response
-        
+                    let response = await AF.request(endPoint)
+                        .validate(statusCode: 200..<300)
+                        .serializingDecodable(T.self, decoder: endPoint.decoder)
+                        .response
 #if DEBUG
-        try NetworkLogger.response(response)
+                    try NetworkLogger.response(response)
 #endif
-        
-        switch response.result {
-        case .success(let value):
-            return value
-        case .failure(let error):
-            guard let data = response.data else {
-                throw error
+                    switch response.result {
+                    case .success(let value):
+                        observer(.success(value))
+                    case .failure(let error):
+                        guard let data = response.data else {
+                            throw error
+                        }
+                        throw endPoint.error(
+                            response.response?.statusCode,
+                            data: data
+                        )
+                    }
+                } catch {
+                    observer(.failure(error))
+                }
             }
-            throw endPoint.error(
-                response.response?.statusCode,
-                data: data
-            )
+            return Disposables.create { task.cancel() }
         }
     }
 }
