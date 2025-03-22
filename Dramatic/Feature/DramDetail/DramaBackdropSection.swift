f71b16a278ed1cdd3c9725aa2235074f6c17a351
김도형 <shapekim98@gmail.com>
2025-03-22 14:44:47 +0900

    [feat] #12 백드롧 섹션 셀 구성

---
+++ Dramatic/Feature/DramDetail/DramaBackdropSection.swift
@@ -0,0 +1,33 @@
+//
+//  DramaBackdropCell.swift
+//  Dramatic
+//
+//  Created by 김도형 on 3/22/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class DramaBackdropSection: BaseCollectionViewCell {
+    private let backdropImageView = UIImageView()
+    
+    override func configureView() {
+        contentView.backgroundColor = .clear
+    }
+    
+    override func configureHierarchy() {
+        contentView.addSubview(backdropImageView)
+    }
+    
+    override func configureLayout() {
+        backdropImageView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(250)
+        }
+    }
+    
+    func registration(url: String) {
+        backdropImageView.setImage(with: url)
+    }
+}
