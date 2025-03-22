f5bfefec3d6198c7dcce5e78ca0b42984f871e65
김도형 <shapekim98@gmail.com>
2025-03-23 00:29:31 +0900

    [fix] #12 드라마 백드록 섹션 이미지 url 수정

--- Dramatic/Feature/DramDetail/DramaBackdropSection.swift
+++ Dramatic/Feature/DramDetail/DramaBackdropSection.swift
@@ -22,12 +22,12 @@ final class DramaBackdropSection: BaseCollectionViewCell {
     
     override func configureLayout() {
         backdropImageView.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview()
+            make.edges.equalToSuperview()
             make.height.equalTo(250)
         }
     }
     
     func registration(url: String) {
-        backdropImageView.setImage(with: url)
+        backdropImageView.setImage(with: "https://image.tmdb.org/t/p/w500\(url)")
     }
 }
