7806e860e8eebbeb0ee9fc6744f9bfa42ed7aab5
김도형 <shapekim98@gmail.com>
2025-03-23 00:30:30 +0900

    [refactor] #12 네트워크 셀 BaseView로 변경

--- Dramatic/Feature/DramDetail/DramaNetworkCell.swift
+++ Dramatic/Feature/DramDetail/DramaNetworkCell.swift
@@ -9,9 +9,17 @@ import UIKit
 
 import SnapKit
 
-final class DramaNetworkCell: BaseCollectionViewCell {
+final class DramaNetworkCell: BaseView {
     private let logoImageView = UIImageView()
     private let chevronImageView = UIImageView()
+    
+    private let url: String
+    
+    init(url: String) {
+        self.url = url
+        
+        super.init(frame: .zero)
+    }
     
     override func configureView() {
         configureContentView()
@@ -22,7 +30,7 @@ final class DramaNetworkCell: BaseCollectionViewCell {
     }
     
     override func configureHierarchy() {
-        contentView.addSubViews(
+        addSubViews(
             logoImageView,
             chevronImageView
         )
@@ -30,29 +38,35 @@ final class DramaNetworkCell: BaseCollectionViewCell {
     
     override func configureLayout() {
         logoImageView.snp.makeConstraints { make in
+            make.height.equalTo(24)
+            make.width.equalTo(60)
             make.verticalEdges.leading.equalToSuperview().inset(12)
         }
         
         chevronImageView.snp.makeConstraints { make in
+            make.size.equalTo(24)
             make.verticalEdges.trailing.equalToSuperview().inset(12)
         }
     }
     
     private func configureContentView() {
-        contentView.backgroundColor = .secondarySystemBackground
-        contentView.layer.cornerRadius = 8
+        backgroundColor = .secondaryLabel
+        layer.cornerRadius = 8
     }
     
     private func configureLogoImageView() {
+        logoImageView.setImage(with: url)
         logoImageView.contentMode = .scaleAspectFit
     }
     
     private func configureChevronImageView() {
         chevronImageView.image = UIImage(systemName: "chevron.right")
         chevronImageView.contentMode = .scaleAspectFit
+        chevronImageView.tintColor = .dt(.semantic(.icon(.secondary)))
     }
-    
-    func registration(url: String) {
-        logoImageView.setImage(with: url)
-    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    DramaDetailViewController()
 }
