ed93bb16acdcb5ca2131850352608fa4b2d21b3c
김도형 <shapekim98@gmail.com>
2025-03-24 15:47:41 +0900

    [feat] #12 뷰 프로토콜 적용

--- Dramatic/Feature/DramDetail/DramaDetailViewController.swift
+++ Dramatic/Feature/DramDetail/DramaDetailViewController.swift
@@ -12,50 +12,39 @@ import ReactorKit
 import RxSwift
 import RxCocoa
 
-final class DramaDetailViewController: BaseViewController<DramaDetailView> {
-    private let viewModel: DramaDetailViewModel
-    private let disposeBag = DisposeBag()
+final class DramaDetailViewController: BaseViewController<DramaDetailView>, View {
+    var disposeBag = DisposeBag()
     
     init(viewModel: DramaDetailViewModel) {
-        self.viewModel = viewModel
-        
         super.init(nibName: nil, bundle: nil)
+        
+        self.reactor = viewModel
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        bindState()
-        
-        bindAction()
-        
-        viewModel.action.onNext(.viewDidLoad)
+        reactor?.action.onNext(.viewDidLoad)
     }
-}
-
-private extension DramaDetailViewController {
-    typealias Action = DramaDetailViewModel.Action
     
-    func bindState() {
-        viewModel.state.map(\.dramaDetail)
+    func bind(reactor: DramaDetailViewModel) {
+        mainView.episodeSectionModelSelected
+            .map { Reactor.Action.episodeSectionModelSelected($0) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map(\.dramaDetail)
             .compactMap(\.self)
             .bind(to: mainView.rx.configureSnapShot)
             .disposed(by: disposeBag)
         
-        viewModel.pulse(\.$showSeasonDetail)
+        reactor.pulse(\.$showSeasonDetail)
             .asDriver(onErrorJustReturn: nil)
             .compactMap(\.self)
             .drive(with: self) { this, season in
                 /// 임시
                 this.navigationController?.pushViewController(UIViewController(), animated: true)
             }
-            .disposed(by: disposeBag)
-    }
-    
-    func bindAction() {
-        mainView.episodeSectionModelSelected
-            .map { Action.episodeSectionModelSelected($0) }
-            .bind(to: viewModel.action)
             .disposed(by: disposeBag)
     }
 }
