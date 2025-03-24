8c2e2f1a9e0902b20400dbc2f691759fb9c786df
been <leebin0603@naver.com>
2025-03-24 21:31:08 +0900

    [feat] #18 Search 화면 로직 구현

--- Dramatic/Feature/Search/SearchViewController.swift
+++ Dramatic/Feature/Search/SearchViewController.swift
@@ -5,4 +5,56 @@
 //  Created by 이빈 on 3/24/25.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import ReactorKit
+
+final class SearchViewController: BaseViewController<SearchView> {
+    
+    var disposeBag = DisposeBag()
+
+    override func configureNavigation() {
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.reactor = SearchViewModel()
+    }
+
+}
+
+extension SearchViewController: View {
+    
+    func bind(reactor: SearchViewModel) {
+        // 검색
+        mainView.searchBar.searchBar.rx.searchButtonClicked
+            .withUnretained(self)
+            .map { owner, _ in (1, owner.mainView.searchBar.searchBar.text) }
+            .map { Reactor.Action.loadSearchData(page: $0.0, keyword: $0.1) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        // 셀 선택
+        mainView.posterCollectionView.rx.modelSelected(DramaDisplayable.self)
+            .map { Reactor.Action.selectDrama(id: $0.id) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        // 결과 데이터 반영
+        reactor.pulse(\.$searchDrama)
+            .bind(to: mainView.posterCollectionView.rx.items(cellIdentifier: PosterCollectionViewCell.identifier, cellType: PosterCollectionViewCell.self)) { (row, element, cell) in
+                cell.imageView.setImage(with: element.imageURL)
+            }
+            .disposed(by: disposeBag)
+        
+        // 셀 선택: 화면 전환
+        reactor.state
+            .map { $0.selectedDrama }
+            .bind(with: self) { owner, id in
+                print("화면전환 아이디: \(id)")
+            }
+            .disposed(by: disposeBag)
+    }
+
+}
--- Dramatic/Feature/Search/SearchViewModel.swift
+++ Dramatic/Feature/Search/SearchViewModel.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import RxSwift
+import ReactorKit
+
+final class SearchViewModel: Reactor {
+    
+    enum Action {
+        case loadSearchData(page: Int, keyword: String?)
+        case selectDrama(id: Int)
+    }
+    
+    enum Mutation {
+        case setSearchDrama(dramas: [DramaDisplayable])
+        case setSelectedDrama(id: Int)
+    }
+    
+    struct State {
+        @Pulse var searchDrama: [DramaDisplayable]
+        var selectedDrama: Int?
+    }
+    
+    let initialState: State
+    
+    init() {
+        self.initialState = State(searchDrama: [])
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .loadSearchData(let page, let keyword):
+            return DramaClient.shared.fetchSearchDrama(page: page, keyword: keyword)
+                .map { Mutation.setSearchDrama(dramas: $0.results.map { $0.toEntity() }) }
+                .asObservable()
+            
+        case .selectDrama(let id):
+            return Observable.just(Mutation.setSelectedDrama(id: id))
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .setSearchDrama(let dramas): newState.searchDrama = dramas
+        case .setSelectedDrama(let id): newState.selectedDrama = id
+        }
+        return newState
+    }
+    
+}
